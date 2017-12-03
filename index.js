24aa1aef250a75139f7b1e0c58ca1bcb7894b36c
Andy Bettisworth <andy@accreu.com>
2017-12-03 12:39:03 -0600

    add task

---
+++ index.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const fs = require('fs')
+const path = require('path')
+const child_process = require('child_process')
+
+/**
+ * Constants
+ */
+
+const base = path.join(__dirname, '..', '..')
+const package_json_path = path.join(base, 'package.json')
+const index_js_path = path.join(base, 'index.js')
+
+/**
+ * Locals
+ */
+
+let main_script
+let has_config = fs.existsSync(package_json_path)
+
+/**
+ * Set the main script to load
+ */
+
+if (has_config == false) {
+  main_script = index_js_path
+} else {
+  let config = JSON.parse(fs.readFileSync(package_json_path))
+  if (config.main === undefined || config.main === '') {
+    main_script = index_js_path
+  } else {
+    main_script = path.resolve(config.main)
+  }
+}
+
+/**
+ * Load the main script
+ */
+
+let has_main_script = fs.existsSync(main_script)
+
+if (has_main_script) {
+  const app = require(main_script)
+
+  /**
+   * Get db interface
+   */
+
+  if (!Object.keys(app.locals).includes('db')) { throw Error("Missing 'db' on app.locals {Object}."); return }
+  const db = app.locals.db
+
+  /**
+   * Get db configuration
+   */
+
+  if (!Object.keys(db).includes('config')) { throw Error("Missing 'config' on app.locals.db {Object}."); return }
+  const keys = Object.keys(db.config)
+  if (!keys.includes('database')) { throw Error("Missing 'database' on app.locals.db.config {Object}."); return }
+  let pg_database = db.config.database
+
+  /**
+   * Create database
+   */
+
+  child_process.spawn('dropdb', [pg_database])
+} else {
+  throw Error("Missing 'main' in package.json and missing an index.js file.")
+}
